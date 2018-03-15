8c101972860f4e7e6375a7f3bc9d589101f6324c
DarioScarfone <DarioScarfone@9.145.4.138>
2018-03-15 15:33:42 +0100

    cIAOOOOO

--- Giardino.java
+++ Giardino.java
@@ -1,50 +1,47 @@
+//INDENTAZIONE
 
-public class Giardino 
-{    private int codice;
-     private String proprietario;
-     private String indirizzo;
-     private int dimensione;
-     private char v;
-     private char w;
-     
-     
-     public Giardino(int codice,String proprietario,String indirizzo,int dimensione,char v,char w)
-     {      this.codice=codice;
-            this.proprietario=proprietario;
-            this.indirizzo=indirizzo;
-            this.dimensione=dimensione;
-            this.v=v;
-            this.w=w;
-     }
-     
-     public int GetCodice()
-     {      return codice;
-    	 
-     }
-     
-     public String GetProprietario()
-     {
-    	    return proprietario;
-     }
-     
-     public String GetIndirizzo()
-     {
-    	    return indirizzo;
-     }
-     
-     public int GetDimensione()
-     {
-    	    return dimensione;
-     }
-     
-     public char Getv()
-     {
-    	    return v;
-     }
-     
-     public char getw()
-     { 
-    	 return w;
-     }
+public class Giardino {
+	private int codice;
+	private String proprietario;
+	private String indirizzo;
+	private int dimensione;
+	private char v;
+	private char w;
+
+	public Giardino(int codice, String proprietario, String indirizzo, int dimensione, char v, char w) {
+		this.codice = codice;
+		this.proprietario = proprietario;
+		this.indirizzo = indirizzo;
+		this.dimensione = dimensione;
+		this.v = v;
+		this.w = w;
+	}
+
+	// I METODI VANNO SCRITTI CON LA LETTERA MINUSCOLA
+	// getCodice non GetCodice, altrimenti getCancer()
+	public int GetCodice() {
+		return codice;
+
+	}
+
+	public String GetProprietario() {
+		return proprietario;
+	}
+
+	public String GetIndirizzo() {
+		return indirizzo;
+	}
+
+	public int GetDimensione() {
+		return dimensione;
+	}
+
+	public char Getv() {
+		return v;
+	}
+
+	public char getw() {
+		return w;
+	}
 
 }
--- Proprietario.java
+++ Proprietario.java
@@ -1,16 +1,18 @@
-public class Proprietario 
-{  private String nome;
-   private String città;
-   
-   public Proprietario(String nome,String città)
-   {   this.nome=nome;
-       this.città=città;
-   }
-   
-   public String getNome()
-   {   return nome;
-   }
-   public String getCittà()
-   {   return città;
-   }
+//INDENTAZIONE DIO SANTO
+public class Proprietario {
+	private String nome;
+	private String città;
+
+	public Proprietario(String nome, String città) {
+		this.nome = nome;
+		this.città = città;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCittà() {
+		return città;
+	}
 }
