75bd408fa4006f335c21464358a841cc8068e548
pmpvincenzo <37399292+pmpvincenzo@users.noreply.github.com>
2018-03-16 20:13:51 +0100

    Update Vivaio.java

--- Vivaio.java
+++ Vivaio.java
@@ -6,40 +6,67 @@ public class Vivaio {
 	List<Proprietario> listaProprietari = new ArrayList<Proprietario>();
 	List<Fiore> listaFiori = new ArrayList<Fiore>();
 	List<Giardino> listaGiardini = new ArrayList<Giardino>();
-	
-	
+
 	public Vivaio(List<Proprietario> listaProprietari, List<Fiore> listaFiori, List<Giardino> listaGiardini) {
 		this.listaProprietari = listaProprietari;
 		this.listaFiori = listaFiori;
 		this.listaGiardini = listaGiardini;
 	}
-	
-	
+
 	public List<Proprietario> getListaProprietari() {
 		return listaProprietari;
 	}
+
 	public List<Fiore> getListaFiori() {
 		return listaFiori;
 	}
+
 	public List<Giardino> getListaGiardini() {
 		return listaGiardini;
 	}
-	
-	public ArrayList<Proprietario> metodo1(double p1, double p2){
+
+	public ArrayList<Proprietario> metodo1(double p1, double p2) {
 		ArrayList<Proprietario> listaProp = new ArrayList<Proprietario>();
-		for(Giardino giard : listaGiardini) {
+		for (Giardino g : listaGiardini) {
 			boolean tuttiFioriCompresi = true;
-			for(Fiore fiore : giard.getV()) {
-				if(!(fiore.getCosto() >= p1 && fiore.getCosto() <= p2)) {
+			for (Fiore fiore : g.getV()) {
+				if (!(fiore.getCosto() >= p1 && fiore.getCosto() <= p2)) {
 					tuttiFioriCompresi = false;
 					break;
 				}
 			}
-			if(tuttiFioriCompresi) {
-				listaProp.add(giard.GetProprietario());
+			if (tuttiFioriCompresi) {
+				listaProp.add(g.getProprietario());
 			}
 		}
 		return listaProp;
-		
+	}
+
+	public ArrayList<Fiore> metodo2(int d, String c){
+		ArrayList<Fiore> listaFiori = new ArrayList<Fiore>();
+		for(Giardino g: listaGiardini) {
+			if (g.getDimensione() == d && cittàGiusta(g.getProprietario.getCittà.eguals(c)){
+				listaFiori.addAll(g.getV());
+			}
+		}
+		return listaFiori;
+	}
+
+		public ArrayList<Giardino> metodo3(){
+    	int numeroFiori = lenght(listaFiori);
+    	ArrayList<Giardino> listaGiardini = new ArrayList<Giardino>();
+        for(Giardino g : listaGiardini) {
+        	if (lenght(numeroFiori==3)) {
+        		if (g.getDimensione() == numeroFiori){
+    	    	    listaGiardini.add(g);
+    	    	    numeroFiori--;
+        		}
+    	        else{
+    	        	numeroFiori--;
+    	        }
+    	    break;
+            }
+        }
+        return numeroFiori;
 	}
 }
